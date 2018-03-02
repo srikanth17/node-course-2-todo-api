ae729959e307068a2fc8fa6c3c9ba181d96258e1
Srikanth Dorairaj <srikanth@Srikanths-MBP.connect>
2018-03-02 15:56:31 +0000

    Add DELETE /todos/:id

---
+++ playground/mongoose-remove.js
@@ -0,0 +1,13 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user');
+
+// Todo.remove({}).then((result) => {
+//     console.log(result);
+// });
+
+Todo.findByIdAndRemove('5a9970fc3f8c9874a1bb7ae4').then((todo) => {
+    console.log(todo);
+});
